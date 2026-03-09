1c38c565e2deddf5cf2cce6742253f590d2a1567
tazrian-amin <amin.tazrian1979@gmail.com>
2026-03-09 17:05:58 +0600

    Refactor ThemeRegistry to enhance cache management and style insertion. Introduced a custom flush method for better handling of inserted styles and improved compatibility with Emotion's cache.

--- app/ThemeRegistry.tsx
+++ app/ThemeRegistry.tsx
@@ -5,23 +5,56 @@ import { useServerInsertedHTML } from "next/navigation";
 import { CacheProvider } from "@emotion/react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
 import theme from "./theme";
-
-const cache = createCache({ key: "mui", prepend: true });
 
 export default function ThemeRegistry({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useServerInsertedHTML(() => (
-    <style
-      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(" ")}`}
-      dangerouslySetInnerHTML={{
-        __html: Object.values(cache.inserted).join(" "),
-      }}
-    />
-  ));
+  const [{ cache, flush }] = useState(() => {
+    const cache = createCache({ key: "mui" });
+    cache.compat = true;
+
+    const prevInsert = cache.insert;
+    let inserted: string[] = [];
+
+    cache.insert = (...args) => {
+      const serialized = args[1];
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name);
+      }
+      return prevInsert(...args);
+    };
+
+    const flush = () => {
+      const prevInserted = inserted;
+      inserted = [];
+      return prevInserted;
+    };
+
+    return { cache, flush };
+  });
+
+  useServerInsertedHTML(() => {
+    const names = flush();
+    if (names.length === 0) {
+      return null;
+    }
+
+    let styles = "";
+    for (const name of names) {
+      styles += cache.inserted[name];
+    }
+
+    return (
+      <style
+        data-emotion={`${cache.key} ${names.join(" ")}`}
+        dangerouslySetInnerHTML={{ __html: styles }}
+      />
+    );
+  });
 
   return (
     <CacheProvider value={cache}>
